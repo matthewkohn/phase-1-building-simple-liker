7f86706cf6e4d77154b511f24c8c38a93d7bc115
Matthew <kohney319m@gmail.com>
2021-10-05 11:58:38 -0600

    commit tree cf6ee788a28b063edb6f98fd90dde420a0fb2637

--- main.js
+++ main.js
@@ -3,8 +3,44 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+// When a user clicks on an empty heart:
+document.addEventListener("DOMContentLoaded", () => {
+  function likeCallback(event) {
+    mimicServerCall()
+    .then(() => handleResponse(event))
+    .catch(error => handleError(error))
+  }
+  likeCallback();
 
+  function handleResponse() {
+  // querySelectorAll returns a Nodelist of like buttons
+    const likeButtons = document.querySelectorAll('.like-glyph');
+    // turns the likeButtons Nodelist into an array and assigns click event listener to each, changing the likeButton that is clicked.
+    for (const button of likeButtons) {
+      button.addEventListener('click', e => {
+        if (button.textContent === EMPTY_HEART) {
+          button.classList.add('activated-heart')
+          button.textContent = FULL_HEART;
+        } else if (button.textContent === FULL_HEART) {
+          button.classList.remove('activated-heart');
+          button.textContent = EMPTY_HEART;
+        }
+      })
+    }
+  }
 
+  function handleError(error) {
+    const modal = document.querySelector('#modal');
+    const modalMessage = document.querySelector('#modal-message');
+    console.log(`ERROR: ${error}`);
+    // Display the error modal by removing the .hidden class
+    modal.classList.remove('hidden');
+    // Display the server error message in the modal
+    modalMessage.textContent = error;
+    // Use setTimeout to hide the modal after 3 seconds
+    setTimeout(() => modal.classList.add('hidden'), 3000);
+  }
+})
 
 
 //------------------------------------------------------------------------------
